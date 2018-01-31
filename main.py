ceb4ea9fe5d46e4bd6b38c1f01bc08b81da825ea
Aleksandr Romanov <aromanov@gotrg.com>
2018-01-31 17:16:13 +0300

    second

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from grab.spider import Spider, Task, Data
 from grab import Grab
 import browsers
-#TRG#cisco_wds
-def get_browser():
-    return browsers.ChromeBrowser()
+
+
+class ShumSpider(Spider):
+
+    def task_generator(self):
+        print('ss')
+        g = Grab()
+
+        yield Task('test', url='https://spb.showgogo.ru/events/festivals/mayatnik-fuko/')
+
+    def task_test(self, grab, task):
+        print('s2')
+
+
+if __name__ == '__main__':
+    bot = ShumSpider(thread_number=10)
+
+    try:
+        bot.run()
+    except KeyboardInterrupt:
+        pass
+    print(bot.render_stats())
---
+++ spider.py
@@ -0,0 +1,6 @@
+from grab.spider import Spider, Task, Data
+from grab import Grab
+
+class ShumSpider(Spider):
+
+    browser = None
